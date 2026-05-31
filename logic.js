1f8c7a7206dab3cea2ac6b1dfcac38bea4a4ba63
RahulGunturu <rahulgunturu123@gmail.com>
2026-05-31 23:00:41 +0530

    Made a rough version of etch-a-sketch

---
+++ logic.js
@@ -0,0 +1,46 @@
+const container = document.querySelector('.container');
+
+function createGrid(numberOfGrids)
+{
+    for(let i = 0 ; i < numberOfGrids ; i++)
+    {
+        const row = document.createElement('div');
+        row.style.display = 'Flex';
+        row.style.flex = "1";
+        row.classList.add('rows');
+
+        for(let j = 0 ; j < numberOfGrids ; j++)
+        {
+            const square = document.createElement('div');
+            square.style.flex = "1";
+            square.style.outline = "1px solid red";
+
+            square.addEventListener('mouseenter',(e) => {
+                e.target.style.backgroundColor = "Black";
+            });
+
+
+            row.appendChild(square);
+        }
+
+        container.appendChild(row);
+    }
+}
+
+createGrid(16);
+
+const setButton = document.querySelector('.set-button');
+let numberOfGrids;
+
+setButton.addEventListener('click', () => {
+    numberOfGrids = prompt('Enter the number of grids you want');
+
+    const rows = document.querySelectorAll('.rows');
+
+    rows.forEach((row) => {
+        container.removeChild(row);
+    })
+
+    createGrid(numberOfGrids);
+});
+
